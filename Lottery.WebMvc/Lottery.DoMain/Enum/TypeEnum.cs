a2e2569ce9d11dbad83797e72b53756d6be313ba
VTI\duc.phamvan <kimconoc@gmail.com>
2023-09-11 17:21:45 +0700

    update code

---
+++ Lottery.WebMvc/Lottery.DoMain/Enum/TypeEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery.DoMain.Enum
+{
+    public enum LotteryEnum
+    {
+        [Description("Lô")]
+        DangXuLy = 0,
+        [Description("Đề")]
+        DangVanChuyen = 1,
+        [Description("Lô xiên")]
+        DonDaGiao = 2,
+    }
+}
