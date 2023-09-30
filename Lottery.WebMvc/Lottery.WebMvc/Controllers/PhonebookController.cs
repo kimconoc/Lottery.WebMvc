a2dc7b7c39b7ed383c48d005165f32b2c8a942d8
Nguyễn Minh Tâm <nguyenminhtamqn1992@gmail.com>
2023-09-30 15:16:41 +0700

    update trang tao moi danh ba

---
+++ Lottery.WebMvc/Lottery.WebMvc/Controllers/PhonebookController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lottery.WebMvc.Controllers
+{
+    public class PhonebookController : BaseController
+    {
+        public ActionResult CreatePlayer()
+        {
+            return View();
+        }
+    }
+}
