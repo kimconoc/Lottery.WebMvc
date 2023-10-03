b80c342def4d0821d9169515d202d9d57fbcc9fd
VTI\duc.phamvan <kimconoc@gmail.com>
2023-10-03 14:48:42 +0700

    push code

--- Lottery.WebMvc/Lottery.WebMvc/Controllers/PhonebookController.cs
+++ Lottery.WebMvc/Lottery.WebMvc/Controllers/PhonebookController.cs
@@ -20,6 +20,19 @@ namespace Lottery.WebMvc.Controllers
         {
             return View();
         }
+        public ActionResult UpdatePlayer(int playerId)
+        {
+            var userData = GetCurrentUser();
+            List<Phonebook> phonebooks = new List<Phonebook>();
+            var dataBase = provider.GetAsync<List<Phonebook>>(string.Format(ApiUri.Get_Phonebook + "/{0}", userData.Id));
+            if (dataBase != null && dataBase.Result != null && dataBase.Result.Data != null)
+            {
+                phonebooks = dataBase.Result.Data;
+            }
+            var phonebook = phonebooks.FirstOrDefault(x => x.Id == playerId);
+
+            return View(phonebook);
+        }
 
         [HttpPost]
         public ActionResult ExecuteCreatePlayer(string playerJsons)
@@ -27,11 +40,11 @@ namespace Lottery.WebMvc.Controllers
             var userData = GetCurrentUser();
             var players = JsonConvert.DeserializeObject<List<PhonebookModel>>(playerJsons);
             players[0].UserID = userData.Id;
-            players[0].IsDeleted = false;
-            players[0].PhoneNumber = "";
             // Tạo mới
             if (players[0].Id == null)
             {
+                players[0].IsDeleted = false;
+                players[0].PhoneNumber = "";
                 var playerBase = provider.PutAsync<object>(ApiUri.POST_UserUpdatePhonebook, players);
                 if (playerBase != null || playerBase.Result != null || playerBase.Result.IsSuccessful)
                 {
@@ -41,24 +54,43 @@ namespace Lottery.WebMvc.Controllers
             // Update
             else if (players[0].Id != null && !players[0].IsDeleted)
             {
+                if (string.IsNullOrEmpty(players[0].PhoneNumber))
+                {
+                    players[0].PhoneNumber = "";
+                }
                 var playerBase = provider.PutAsync<object>(ApiUri.POST_UserUpdatePhonebook, players);
                 if (playerBase != null || playerBase.Result != null || playerBase.Result.IsSuccessful)
                 {
                     return Json(Success_Request(playerBase.Result.IsSuccessful));
                 }
             }
-            // Xóa
-            else if (players[0].Id != null && players[0].IsDeleted)
-            {
-                var playerBase = provider.PutAsync<object>(ApiUri.POST_UserUpdatePhonebook, players);
-                if (playerBase != null || playerBase.Result != null || playerBase.Result.IsSuccessful)
-                {
-                    return Json(Success_Request(playerBase.Result.IsSuccessful));
-                }
-            }    
-
+            
             return View(Server_Error());
 
+        }
+        [HttpPost]
+        public ActionResult ExecuteDeletePlayer(int playerId)
+        {
+            var userData = GetCurrentUser();
+            List<Phonebook> phonebooks = new List<Phonebook>();
+            var dataBase = provider.GetAsync<List<Phonebook>>(string.Format(ApiUri.Get_Phonebook + "/{0}", userData.Id));
+            if (dataBase != null && dataBase.Result != null && dataBase.Result.Data != null)
+            {
+                phonebooks = dataBase.Result.Data;
+            }
+            var phonebook = phonebooks.FirstOrDefault(x => x.Id == playerId);
+            phonebook.IsDeleted = true;
+            var players = new List<Phonebook>();
+            players.Add(phonebook);
+
+            // Xóa
+            var playerBase = provider.PutAsync<object>(ApiUri.POST_UserUpdatePhonebook, players);
+            if (playerBase != null || playerBase.Result != null || playerBase.Result.IsSuccessful)
+            {
+                return Json(Success_Request(playerBase.Result.IsSuccessful));
+            }
+
+            return View(Server_Error());
         }
     }
 }
